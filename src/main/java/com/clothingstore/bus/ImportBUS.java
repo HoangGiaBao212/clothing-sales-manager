34d0b8f9967c758042e70b5f6f438d282c6d502a
Nguyễn Anh Danh <danhanh.nguyen1643@gmail.com>
2023-11-24 12:26:44 +0700

    add checking quantity when clicking product detail

--- src/main/java/com/clothingstore/bus/ImportBUS.java
+++ src/main/java/com/clothingstore/bus/ImportBUS.java
@@ -1,6 +1,5 @@
 package com.clothingstore.bus;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
--- src/main/java/com/clothingstore/gui/components/Product.java
+++ src/main/java/com/clothingstore/gui/components/Product.java
@@ -7,13 +7,20 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.*;
+
+import com.clothingstore.bus.SizeItemBUS;
 import com.clothingstore.models.OrderItemModel;
 import com.clothingstore.models.ProductModel;
+import com.clothingstore.models.SizeItemModel;
 
 public class Product extends JPanel {
+  private static int sizeSQuantity = 1;
+  private static int sizeMQuantity = 1;
+  private static int sizeLQuantity = 1;
+  private static int sizeXLQuantity = 1;
+  private static int sizeXXLQuantity = 1;
   public static List<OrderItemModel> cartItems = new ArrayList<>();
   DecimalFormat decimalFormat = new DecimalFormat("###,###");
-
 
   public Product(ProductModel productModel) {
     initComponents(productModel);
@@ -32,29 +39,29 @@ public class Product extends JPanel {
     SwingWorker<ImageIcon, Void> worker = new SwingWorker<>() {
       @Override
       protected ImageIcon doInBackground() throws Exception {
-          ImageIcon icon = new ImageIcon(productModel.getImage());
-          Image image = icon.getImage();
-          Image scaledImage = image.getScaledInstance(180, 180, Image.SCALE_SMOOTH);
-          return new ImageIcon(scaledImage);
+        ImageIcon icon = new ImageIcon(productModel.getImage());
+        Image image = icon.getImage();
+        Image scaledImage = image.getScaledInstance(180, 180, Image.SCALE_SMOOTH);
+        return new ImageIcon(scaledImage);
       }
 
       @Override
       protected void done() {
-          try {
-              ImageIcon scaledIcon = get();
-              JLabel imageLabel = new JLabel(scaledIcon);
-              imageLabel.setPreferredSize(new Dimension(180, 178));
-              header.add(imageLabel, new GridBagConstraints());
-              add(header, BorderLayout.CENTER);
-              revalidate();
-              repaint();
-          } catch (Exception e) {
-              e.printStackTrace();
-          }
+        try {
+          ImageIcon scaledIcon = get();
+          JLabel imageLabel = new JLabel(scaledIcon);
+          imageLabel.setPreferredSize(new Dimension(180, 178));
+          header.add(imageLabel, new GridBagConstraints());
+          add(header, BorderLayout.CENTER);
+          revalidate();
+          repaint();
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
       }
-  };
+    };
 
-  worker.execute();
+    worker.execute();
 
     JPanel footer = new JPanel();
     footer.setBackground(new Color(255, 255, 255));
@@ -84,6 +91,44 @@ public class Product extends JPanel {
 
     @Override
     public void mouseClicked(MouseEvent e) {
+      List<SizeItemModel> sizeItemModels = new ArrayList<>(
+          SizeItemBUS.getInstance().searchModel(String.valueOf(productModel.getId()), new String[] { "product_id" }));
+      for (int i = 0; i < sizeItemModels.size(); i++) {
+        if (sizeItemModels.get(i).getSizeId() == 1 && sizeItemModels.get(i).getQuantity() < 1) {
+          sizeSQuantity = 0;
+        }
+        if (sizeItemModels.get(i).getSizeId() == 2 && sizeItemModels.get(i).getQuantity() < 1) {
+          sizeMQuantity = 0;
+        }
+        if (sizeItemModels.get(i).getSizeId() == 3 && sizeItemModels.get(i).getQuantity() < 1) {
+          sizeLQuantity = 0;
+        }
+        if (sizeItemModels.get(i).getSizeId() == 4 && sizeItemModels.get(i).getQuantity() < 1) {
+          sizeXLQuantity = 0;
+        }
+        if (sizeItemModels.get(i).getSizeId() == 5 && sizeItemModels.get(i).getQuantity() < 1) {
+          sizeXXLQuantity = 0;
+        }
+
+      }
+      if (sizeSQuantity == 0 && sizeMQuantity == 0 && sizeLQuantity == 0 && sizeXLQuantity == 0
+          && sizeXXLQuantity == 0) {
+        JFrame jf = new JFrame();
+        jf.setAlwaysOnTop(true);
+        JOptionPane.showMessageDialog(jf, "Sản phẩm đã hết hàng!");
+        sizeSQuantity = 1;
+        sizeMQuantity = 1;
+        sizeLQuantity = 1;
+        sizeXLQuantity = 1;
+        sizeXXLQuantity = 1;
+        return;
+      }
+      if (productModel.getStatus() == 0) {
+        JFrame jf = new JFrame();
+        jf.setAlwaysOnTop(true);
+        JOptionPane.showMessageDialog(jf, "Sản phẩm tạm thời không tồn tại!");
+        return;
+      }
       ProductDetail productDetail = new ProductDetail(productModel);
       productDetail.setVisible(true);
     }
